3d1c6814ee5718d01bc537131d38e922f2171fb4
Vishnu Goyal <goelvis7@gmail.com>
2016-10-11 13:03:21 +0530

    Update hanoi.c

--- hanoi.c
+++ hanoi.c
@@ -1,11 +1,25 @@
+// in a tower of hanoi problem disks can be placed in decreasing manner from bottom to top
+//there are three towers A,B,C.
+//initially 'n' disks are mounted on permissible manner on tower A.
+//objective is to transfer it to tower C.
+//thus problem statement is transfer disks from A to C via B
 #include<stdio.h>
 void towers(int ,char  ,char  ,char );
+int steps(int m)
+{int x;
+ if(m==2)
+   x=3;
+ else
+x=(2*steps(m-1)+1);
+
+return x;}
 int main(void)
 {
 int n;
-printf("GIven n:");
+printf("Enter the number of disks n:");
 scanf("%d",&n);
-towers(n,'A','B','C');
+printf("Total number of steps required are :%d\nOptimal procedure is:\n",steps(n));  
+towers(n,'A','C','B');
 
 return 0;
 }
@@ -24,3 +38,5 @@ printf("move from %c to %c \n",from,to);
 towers(m-1,via,to,from);
 }
 }
+//updates by aaanant
+//1300 hrs ist
